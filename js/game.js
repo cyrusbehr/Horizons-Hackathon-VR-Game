c43bc73f7b9900befdb0cb3cb57b7f4fac82d160
donfour <donovanso@yahoo.com.hk>
2017-07-15 03:48:39 -0700

    fixed typo

--- js/game.js
+++ js/game.js
@@ -1,6 +1,7 @@
 // gamestart btn animation
 document.getElementById('start_game_btn').addEventListener('mouseover', function(){
-  this.innerHTML = '<i id="start_game_btn" class="fa fa-rocket" style="color:white"></i>';
+  // this.innerHTML = '<i class="fa fa-rocket" style="color:white"></i>';
+  this.textContent = '<i class="fa fa-rocket" style="color:white"></i>';
 })
 
 document.getElementById('start_game_btn').addEventListener('mouseout', function(){
